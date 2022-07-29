2c22a209540a7a8ef039e136912bcc981a28115f
George <subingeorge027@gmail.com>
2022-07-29 23:03:15 +0530

    api-start

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import axios from 'axios';
 function App() {
-  return (
-    <>
+
+  const [advice , setAdvice] = useState('');
+
+
+
+ const fetchAdvice=()=>{
+     axios.get('https://api.adviceslip.com/advice')
+     .then((res)=>{
+        console.log(res.data.slip.advice);
+        setAdvice(res.data.slip.advice)
+     })
+     .catch((error)=>{
+        console.log(error);
+     });
+     
     
+    }
+    
+    
+  return (
+    
+    <>
+      <div>
+        <h1>{advice}</h1>
+        <button onClick={fetchAdvice}>click me</button>
+      </div>
     </>
   )
-}
+}  
 
 export default App
