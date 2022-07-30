3400d8156f9f42083a618b4fc5fc81becfb22578
George <subingeorge027@gmail.com>
2022-07-30 16:36:21 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -1,34 +1,44 @@
 import React, { useState } from 'react'
 import axios from 'axios';
+import sample from './A man swimming and exploring the bottom of the mid-ocean ridge.mp4';
 function App() {
 
-  const [advice , setAdvice] = useState('');
+
+  const [advice, setAdvice] = useState('');
 
 
 
- const fetchAdvice=()=>{
-     axios.get('https://api.adviceslip.com/advice')
-     .then((res)=>{
+  const fetchAdvice = () => {
+    axios.get('https://api.adviceslip.com/advice')
+      .then((res) => {
         console.log(res.data.slip.advice);
         setAdvice(res.data.slip.advice)
-     })
-     .catch((error)=>{
+      })
+      .catch((error) => {
         console.log(error);
-     });
-     
-    
-    }
-    
-    
+      });
+
+
+  }
+
+
   return (
-    
+
     <>
-      <div>
-        <h1>{advice}</h1>
-        <button onClick={fetchAdvice}>click me</button>
+    <div className='navbar nav-bmi' >Advice Generator</div>
+   
+      <div className='container'>
+      
+      <video className='videoTag' autoPlay loop muted>
+          <source src={sample} type='video/mp4' />
+        </video>
+        <div className='advice'>
+          <h1 className='advice-header'>{advice}</h1>
+          <button className='advice-button' onClick={fetchAdvice}>click me</button>
+        </div>
       </div>
     </>
   )
-}  
+}
 
 export default App
